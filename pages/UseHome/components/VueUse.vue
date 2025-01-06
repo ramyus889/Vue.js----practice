5cbb22b51917cda539d9d823eee657ed5b1b0073
ramyus889 <ramyus889.com>
2025-01-06 13:17:09 +0300

    Обновить компонент VueUse: добавить реактивные поля для имени и возраста пользователя, использовать reactiveOmit для исключения возраста из объекта. Исправить пути импорта в маршрутах.

--- pages/UseHome/components/VueUse.vue
+++ pages/UseHome/components/VueUse.vue
@@ -1,84 +1,38 @@
-<script setup>
-import { TransitionPresets, useTransition } from '@vueuse/core';
-import { rand } from '@vueuse/shared';
-import { ref } from 'vue';
-
-const duration = 1500;
-
-const baseNumber = ref(0);
-
-const baseVector = ref([0, 0]);
-
-function easeOutElastic(n) {
-  return n === 0
-    ? 0
-    : n === 1
-      ? 1
-      : 2 ** (-10 * n) * Math.sin((n * 10 - 0.75) * ((2 * Math.PI) / 3)) + 1;
-}
-
-const cubicBezierNumber = useTransition(baseNumber, {
-  duration,
-  transition: [0.75, 0, 0.25, 1]
-});
-
-const customFnNumber = useTransition(baseNumber, {
-  duration,
-  transition: easeOutElastic
-});
-
-const vector = useTransition(baseVector, {
-  duration,
-  transition: TransitionPresets.easeOutExpo
-});
-
-function toggle() {
-  baseNumber.value = baseNumber.value === 100 ? 0 : 100;
-  baseVector.value = [rand(0, 100), rand(0, 100)];
-}
-</script>
-
 <template>
-  <div>
-    <v-btn @click="toggle">Transition</v-btn>
-
-    <p class="mt-2">
-      Cubic bezier curve: <b>{{ cubicBezierNumber.toFixed(2) }}</b>
-    </p>
-
-    <div class="w-full h-4 max-w-xs px-2 my-2 rounded bg-slate-400">
-      <div class="relative h-full">
-        <div
-          class="absolute w-4 h-4 transform -translate-x-1/2 bg-blue-500 rounded-full"
-          :style="{ left: `${cubicBezierNumber}%` }"
-        />
-      </div>
+  <div class="p-4">
+    <h1 class="mb-4 text-2xl font-bold">Пример использования reactiveOmit</h1>
+    <div class="mb-4">
+      <label class="block mb-2">Имя:</label>
+      <input v-model="user.name" class="p-2 border border-gray-300 rounded" />
     </div>
-
-    <p class="mt-2">
-      Custom function: <b>{{ customFnNumber.toFixed(2) }}</b>
-    </p>
-
-    <div class="w-full h-4 max-w-xs px-2 my-2 rounded bg-slate-400">
-      <div class="relative h-full">
-        <div
-          class="absolute w-4 h-4 transform -translate-x-1/2 bg-blue-500 rounded-full"
-          :style="{ left: `${customFnNumber}%` }"
-        />
-      </div>
+    <div class="mb-4">
+      <label class="block mb-2">Возраст:</label>
+      <input v-model="user.age" type="number" class="p-2 border border-gray-300 rounded" />
     </div>
-
-    <p class="mt-2">
-      Vector: <b>[{{ vector[0].toFixed(2) }}, {{ vector[1].toFixed(2) }}]</b>
-    </p>
-
-    <div class="w-full max-w-xs p-2 rounded bg-slate-400">
-      <div class="relative pb-[30%]">
-        <div
-          class="absolute w-4 h-4 transform -translate-x-1/2 -translate-y-1/2 bg-blue-500 rounded-full"
-          :style="{ left: `${vector[0]}%`, top: `${vector[1]}%` }"
-        />
-      </div>
+    <div class="flex mt-4 text-lg">
+      Имя: {{ user.name }}
+      <div v-if="user.name.length >= 7" class="px-1">Developer</div>
     </div>
+    <div class="text-lg">
+      Возраст: {{ user.age }}
+      <div v-if="user.age >= 30" class="px-1">Senior</div>
+      <div v-else-if="user.age >= 26" class="px-1">Middle</div>
+      <div v-else class="px-1">Junior</div>
+    </div>
+    <p class="text-lg">Данные без возраста: {{ userWithoutAge }}</p>
   </div>
 </template>
+
+<script setup>
+import { reactiveOmit } from '@vueuse/core';
+import { reactive } from 'vue';
+
+// Создаем реактивный объект пользователя
+const user = reactive({
+  name: '',
+  age: 0
+});
+
+// Используем reactiveOmit для создания нового объекта без свойства age
+const userWithoutAge = reactiveOmit(user, 'age');
+</script>
